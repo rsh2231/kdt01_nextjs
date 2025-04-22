dbd5d95c78faa672c58eb0e0f4d6de4fb00c1f5d
rsh2231 <rsh2231@naver.com>
2025-04-22 10:10:51 +0900

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// ust client가 없으면 모두 서버에서 실행된다.
 'use client'
 
 import { useAtom } from "jotai";
@@ -16,6 +17,7 @@ export default function Home() {
             <div className="flex min-h-full flex-col justify-center items-center px-6 py-12 lg:px-8">
               <h1 className="flex justify-center items-center w-150 h-30 rounded-2xl text-2xl font-bold bg-gray-100 shadow-2xl">
                 <span className=" text-blue-700">
+                  {/* 서버에는 localStorage가 없다 */}
                   {`${localStorage.getItem('email')}`}
                 </span>
                 님 로그인이 되었습니다.
